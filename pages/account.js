1aae7527d9286d822a19072da61a63e166277363
ahsanhyder <ahsan@sugarcosmetics.com>
2021-02-09 14:36:39 +0530

    initial commit

---
+++ pages/account.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import { useState} from 'react';
+import styles from '../styles/Account.module.css';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+export default function Account({ data }) {
+	console.log('accountPage');
+
+
+	return (
+		<div>
+			<div>
+				<Head>
+					<title>Create Next App</title>
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+			</div>
+			<div style={{ overflowX: 'hidden' }}>
+
+            </div>
+            </div>
+            )}
